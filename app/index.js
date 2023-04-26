54e482aa7c2518816bfeec9adeba9ff6c8e67946
SamuelOI <95894328+blackkiverson@users.noreply.github.com>
2023-04-26 09:26:41 +0100

    handling user cards

--- app/index.js
+++ app/index.js
@@ -1,34 +1,59 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, Image, ImageBackground } from 'react-native'
+import users from '../Asset Bundle OnlyFans/assets/data/users'
+
+const user = users[0];
 
 export default function Page() {
     return (
-        <View style={styles.container}>
-            <View style={styles.main}>
-                <Text style={styles.title}>Hello there</Text>
-                <Text style={styles.subtitle}>This is the first page of your app.</Text>
-            </View>
-        </View>
-    )
+      <View style={styles.container}>
+        {/* Background Image*/}
+        <ImageBackground source={{uri : user.coverImage}} style={styles.userCard}><View style={styles.overlay}/>
+          {/* Image */}
+          <Image
+            src={user.avatar}
+            style={styles.userImage}
+          />
+
+          {/* Name & handle */}
+          <View>
+            <Text style={{color: 'white', fontSize: 22, fontWeight: '500', marginBottom: 5}}>{user.name}</Text>
+            <Text style={{color: 'white'}}>@{user.handle}</Text>
+          </View>
+        </ImageBackground>
+      </View>
+    );
 }
 
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        alignItems: 'center',
-        padding: 24,
+        padding: 10,
+        paddingTop: 75
     },
-    main: {
-        flex: 1,
-        justifyContent: 'center',
-        maxWidth: 960,
-        marginHorizontal: 'auto',
+    userCard: {
+        backgroundColor: 'gray',
+        padding: 10,
+        flexDirection: 'row',
+        alignItems: 'flex-end',
+
+        borderRadius: 5,
+        overflow: 'hidden',
     },
-    title: {
-        fontSize: 64,
-        fontWeight: 'bold',
+    overlay: {
+        backgroundColor: 'rgba(0, 0, 0, 0.5)',
+        // position: 'absolute',
+        // top: 0,
+        // bottom: 0,
+        // left: 0,
+        // right: 0,
+        ...StyleSheet.absoluteFillObject,
     },
-    subtitle: {
-        fontSize: 36,
-        color: '#38434D',
+    userImage: {
+        width: 100,
+        height: 100,
+        borderRadius: 100,
+        borderColor: 'white',
+        borderWidth: 3,
+        marginRight: 20,
     }
 })
