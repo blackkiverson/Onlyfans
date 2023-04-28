e0837d09dd0e2859530986170bf80461292b1946
SamuelOI <95894328+blackkiverson@users.noreply.github.com>
2023-04-28 05:25:43 +0100

    user data linked to user profile page

--- app/user/[id].js
+++ app/user/[id].js
@@ -1,13 +1,20 @@
 import { View, Text } from 'react-native'
 import { useRouter, useSearchParams } from 'expo-router'
+import users from '../../Asset Bundle OnlyFans/assets/data/users';
 
 const ProfilePage = () => {
-    const router = useRouter();
-    const { id } = useSearchParams();
+  const router = useRouter();
+  const { id } = useSearchParams();
+
+  const user = users.find((u) => u.id === id);
+
+  if (!user) {
+    return <Text> User not found!</Text>;
+  }
 
   return (
     <View style={{ marginTop: 100 }}>
-      <Text>Profile Page: {id}</Text>
+      <Text>Profile Page: {user.name}</Text>
       {/* going to the previous page using onPress router.back() */}
       <Text onPress={() => router.back()}>Go Back</Text>
     </View>
