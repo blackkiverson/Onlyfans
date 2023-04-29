3136a1e26b7968ede0cdeb6adf77ec7e59125a29
SamuelOI <95894328+blackkiverson@users.noreply.github.com>
2023-04-29 12:31:07 +0100

    integrating a pay wall incase user isnt subscribed

--- app/user/[id].js
+++ app/user/[id].js
@@ -5,6 +5,7 @@ import { useState } from 'react';
 import UserProfileHeader from '../../src/components/UserProfileHeader';
 import posts from '../../Asset Bundle OnlyFans/assets/data/posts';
 import Post from '../../src/components/Post';
+import { FontAwesome5 } from "@expo/vector-icons";
 
 const ProfilePage = () => {
   // subscription useState created for dynamic button and text interaction
@@ -17,6 +18,42 @@ const ProfilePage = () => {
 
   if (!user) {
     return <Text> User not found!</Text>;
+  }
+
+  if (!isSubscribed) {
+    return (
+      <View>
+        <UserProfileHeader
+          user={user}
+          isSubscribed={isSubscribed}
+          setIsSubscribed={setIsSubscribed}
+        />
+
+        <View
+          style={{
+            backgroundColor: "gainsboro",
+            alignItems: "center",
+            padding: 20,
+          }}
+        >
+          <FontAwesome5 name="lock" size={50} color="gray" />
+          <Text
+            style={{
+              backgroundColor: "royalblue",
+              height: 50,
+              borderRadius: 25,
+              overflow: "hidden",
+              padding: 15,
+              color: 'white',
+              margin: 20
+            }}
+          >
+            {" "}
+            Subscribe to see user's post{" "}
+          </Text>
+        </View>
+      </View>
+    );
   }
 
   return (
