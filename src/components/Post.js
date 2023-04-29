5bc5137744470d5c69d280eee3e9a57fbcb4a153
SamuelOI <95894328+blackkiverson@users.noreply.github.com>
2023-04-29 11:41:50 +0100

    adding icons to the posts

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,13 +1,70 @@
 import { View, Text, Image } from 'react-native'
+import { Entypo } from "@expo/vector-icons";
+import { AntDesign } from "@expo/vector-icons";
+import { FontAwesome5 } from "@expo/vector-icons";
 
 const Post = ({ post }) => {
   return (
-    <View>
-      <Text>Post</Text>
+    <View style={{ marginVertical: 15 }}>
+      <View style={{ flexDirection: "row", alignItems: "center", padding: 5 }}>
+        <Image
+          src={post.User.avatar}
+          style={{
+            width: 50,
+            aspectRatio: 1,
+            borderRadius: 50,
+            marginRight: 10,
+          }}
+        />
+        <View>
+          <Text style={{ fontWeight: "600", marginBottom: 3, fontSize: 16 }}>
+            {post.User.name}
+          </Text>
+          <Text style={{ color: "gray" }}>@{post.User.handle}</Text>
+        </View>
 
-      <Image src={post.image} style={{ width: '100%', aspectRatio: 1}}/>
+        <View
+          style={{
+            marginLeft: "auto",
+            flexDirection: "row",
+            alignItems: "center",
+          }}
+        >
+          <Text style={{ marginRight: 5, color: "gray" }}>3 hours ago</Text>
+          <Entypo
+            name="dots-three-horizontal"
+            size={24}
+            color="gray"
+            style={{ padding: 5 }}
+          />
+        </View>
+      </View>
+
+      <Text style={{ margin: 10, lineHeight: 18 }}>{post.text}</Text>
+
+      <Image src={post.image} style={{ width: "100%", aspectRatio: 1 }} />
+
+      <View style={{ margin: 10, flexDirection: "row" }}>
+        <AntDesign
+          name="hearto"
+          size={20}
+          color="gray"
+          style={{ marginRight: 15 }}
+        />
+
+        <FontAwesome5
+          name="dollar-sign"
+          size={20}
+          color="gray"
+          style={{ marginRight: 15 }}
+        />
+      </View>
+
+      <Text style={{ fontWeight: "500", marginHorizontal: 10 }}>
+        {post.likes} Likes
+      </Text>
     </View>
-  )
+  );
 }
 
 export default Post
